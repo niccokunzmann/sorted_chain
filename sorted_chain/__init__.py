b7fdd70bd315ae150353e67c23ec18ec1db5df13
Nicco Kunzmann <niccokunzmann@rambler.ru>
2024-08-26 18:35:27 +0200

    make moduel

---
+++ sorted_chain/__init__.py
@@ -0,0 +1,104 @@
+"""
+itertools.chain with sorted result
+
+This module provides a single function, `sorted_chain`, which takes any
+number of iterables and returns a single iterable that yields the elements
+from the input iterables in sorted order.
+
+The function is designed to be used with other functions from the
+`itertools` module, such as `groupby` or `sorted`.
+"""
+from __future__ import annotations
+import heapq
+
+from typing import Any, Callable, Iterable
+from functools import total_ordering
+
+@total_ordering
+class _HeapEntry:
+    """
+    A class representing an item in the heap. The heap is sorted by the
+    `key` attribute, and then by the `index` attribute.
+    """
+
+    __slots__ = ('value', 'index', 'iterable', 'key')
+    
+    def __init__(self, value: Any, index: int, iterable: Iterable[Any], key: Any):
+        """
+        Initialize a `HeapEntry` object.
+
+        :param value: The value to be stored in the heap entry.
+        :param index: The index of the iterable that the value comes from.
+        :param iterable: The iterable that the value comes from.
+        :param key: The value to be used for sorting.
+        """
+        self.value = value
+        self.index = index
+        self.iterable = iterable
+        self.key = key
+
+    def __lt__(self, other:_HeapEntry) -> bool:
+        """
+        Compare two heap entries.
+
+        The comparison is first done by the `value` attribute, and then by
+        the `index` attribute.
+        """
+        if self.key < other.key:
+            return True
+        elif self.key > other.key:
+            return False
+        else:
+            return self.index < other.index
+
+    def __eq__(self, other:_HeapEntry) -> bool:
+        """
+        Check if two heap entries are equal.
+
+        The comparison is done by the `key` and `index` attributes.
+        """
+        return self.key == other.key and self.index == other.index
+    
+    def __hash__(self) -> int:
+        """
+        Return a hash of the heap entry.
+
+        The hash is computed from the `key` and `index` attributes.
+        """
+        return hash((self.key, self.index))
+
+def sorted_chain(*iterables: Iterable[Any], key: Callable[[Any], Any] = None, reverse: bool = False) -> Iterable[Any]:
+    """Yield the elements from the endless iterables in sorted order.
+
+    :param iterables: The iterables to be sorted.
+    :param key: A function of one argument that is used to extract a
+        comparison key from each element in the iterable. The default
+        value is None, which means that the elements themselves will be
+        used as the comparison key.
+    :param reverse: A boolean value indicating whether the elements should
+        be sorted in reverse order. The default value is False which implies 
+        that the lowest value is returned first.
+    :return: An iterable of the elements from the input iterables in sorted
+        order.
+    """
+    key = key or (lambda x: x)
+    heap: list[_HeapEntry] = []
+    for i, it in enumerate(iterables):
+        try:
+            first_element = next(it)
+        except StopIteration:
+            pass
+        else:
+            heapq.heappush(heap, _HeapEntry(first_element, i, it, key(first_element)))
+
+    while heap:
+        element = heapq.heappop(heap).value
+        yield element
+        try:
+            next_element = next(heap[-1].iterable)
+        except StopIteration:
+            pass
+        else:
+            heapq.heappush(heap, _HeapEntry(next_element, heap[-1].index, heap[-1].iterable, key(next_element)))
+
+__all__ = ['sorted_chain']
